516c3cc684ae3bf4783e5641a8759b06688bcb32
nikhilverma778 <nikhil@flashaid.in>
2026-05-28 17:41:47 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-5xl font-bold">
+      Revised Flow 🚀
+    </div>
+  );
+}
+
+export default App;
